827e07fc2bb4293746c6c04daf7402af071358ba
Stefano Romanini <spromanini@miuandes.cl>
2024-05-02 16:42:24 -0400

    Added blind gaze and its movement

---
+++ AxyTheBarbarian-grupo2/Assets/Scripts/BlindGazerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlindGazerMovement : MonoBehaviour
+{
+
+    public float amplitude = 3.0f;
+    public float speed = 1.5f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // Calculate how much it moves on the y axis.
+        // Used sin function for a better movement.
+        float verticalOffset = Mathf.Sin(Time.time * speed) * amplitude;
+
+        // Calculate new position based on the starting position and the added value using the sin wave.
+        Vector3 newPosition = startPos + Vector3.up * verticalOffset;
+
+        transform.position = newPosition;
+    }
+}
