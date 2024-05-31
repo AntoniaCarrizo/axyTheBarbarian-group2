97dd35855f2242c53dc02fe9abc73ae4d4bffaac
Stefano Romanini <spromanini@miuandes.cl>
2024-05-31 11:27:53 -0400

    changed blind gazer movement and made map factory base script

--- AxyTheBarbarian-grupo2/Assets/Scripts/BlindGazerMovement.cs
+++ AxyTheBarbarian-grupo2/Assets/Scripts/BlindGazerMovement.cs
@@ -7,6 +7,8 @@ public class BlindGazerMovement : MonoBehaviour
     public float distance = 3.0f;
     public float speed = 1.0f;
     private Vector3 startPos;
+    private float totalVerticalDistance = 0.0f;
+    private int direction = 1;
 
     void Start()
     {
@@ -15,18 +17,21 @@ public class BlindGazerMovement : MonoBehaviour
 
     void Update()
     {
-        CalculateVerticalOffset();
         UpdatePosition();
-    }
-
-    void CalculateVerticalOffset()
-    {
-        float verticalOffset = Mathf.Sin(Time.time * speed) * distance;
-        transform.position = startPos + Vector3.up * verticalOffset;
     }
 
     void UpdatePosition()
     {
-        transform.position += Vector3.right * Time.deltaTime; // Por ejemplo, mueve también en el eje X
+        float verticalOffset = direction * speed * Time.deltaTime;
+        transform.position += Vector3.up * verticalOffset;
+
+        totalVerticalDistance += Mathf.Abs(verticalOffset);
+
+        if (totalVerticalDistance >= distance)
+        {
+            direction *= -1;
+
+            totalVerticalDistance = 0.0f;
+        }
     }
 }
